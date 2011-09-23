739a70925ed9d3d10e208f4a0578e55c7ee9d6fd
Keegan McAllister <mcallister.keegan@gmail.com>
2011-09-23 05:18:19 -0400

    doc

--- System/Posix/Escape.hs
+++ System/Posix/Escape.hs
@@ -1,3 +1,6 @@
+-- | Quote ASCII arguments to be passed through the Unix shell.
+--
+-- For safety, these functions drop all non-ASCII characters.
 module System.Posix.Escape
     ( escape
     , escapeMany
@@ -7,8 +10,12 @@ import qualified System.Posix.Escape.Unicode as U
 
 import Data.Char
 
+-- | Wrap a @String@ so it can be used within a Unix shell command line, and
+-- end up as a single argument to the program invoked.
 escape :: String -> String
 escape = U.escape . filter isAscii
 
+-- | Wrap some @String@s as separate arguments, by inserting spaces before and
+-- after each.  This will break if, for example, prefixed with a backslash.
 escapeMany :: [String] -> String
 escapeMany = U.escapeMany . map (filter isAscii)
--- System/Posix/Escape/Unicode.hs
+++ System/Posix/Escape/Unicode.hs
@@ -1,13 +1,38 @@
+{- | Quote Unicode arguments to be passed through the Unix shell.
+
+If you are escaping ASCII-only strings, use @System.Posix.Escape@ as a safer
+alternative.
+
+If you are escaping untrusted input, you must guarantee that the Unicode
+characters of the escaped @String@ will be serialized using the character
+encoding expected by @\/bin\/sh@.
+
+Some software incorrectly interprets characters as bytes, and will use only the
+low 8 bits of each Unicode code point.  This includes version 1.0 of the
+Haskell @process@ package, which is bundled with GHC 7.0.  Under such
+circumstances this module /will not/ prevent malicious input from escaping the
+quotation.
+
+To repeat: Escaping untrusted input using this module and passing it to the
+@process@ package in GHC 7.0 is NOT SAFE and can allow MALICIOUS CODE
+EXECUTION.  Use @System.Posix.Escape@ as a safer alternative.
+
+-}
+
 module System.Posix.Escape.Unicode
     ( escape
     , escapeMany
     ) where
 
+-- | Wrap a @String@ so it can be used within a Unix shell command line, and
+-- end up as a single argument to the program invoked.
 escape :: String -> String
 escape xs = "'" ++ concatMap f xs ++ "'" where
     f '\0' = ""
     f '\'' = "'\"'\"'"
     f x    = [x]
 
+-- | Wrap some @String@s as separate arguments, by inserting spaces before and
+-- after each.  This will break if, for example, prefixed with a backslash.
 escapeMany :: [String] -> String
 escapeMany xs = " " ++ unwords (map escape xs) ++ " "
